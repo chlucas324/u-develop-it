e2282e2033d13ba40bf20339313e0ca4ab302816
chlucas324 <chlucas324@gmail.com>
2022-05-14 12:43:29 -0400

    update with snapshot

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,10 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  CONSTRAINT fk_party
+    FOREIGN KEY (party_id)
+    REFERENCES parties(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE voters (
@@ -27,11 +30,11 @@ CREATE TABLE voters (
 );
 
 CREATE TABLE votes (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   voter_id INTEGER NOT NULL,
   candidate_id INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-   CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT uc_voter UNIQUE (voter_id),
   CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
